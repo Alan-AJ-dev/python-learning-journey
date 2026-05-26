771fe8bc45f25f953cbc62162fcbbeb5654fa245
Alan Joseph <alanjoseph52006@gmail.com>
2026-05-26 15:33:12 +0530

    Add quiz questions data

---
+++ Oops/quiz-game-start/data.py
@@ -0,0 +1,31 @@
+question_data = [
+{"text": "A slug's blood is green.", "answer": "True"},
+{"text": "The loudest animal is the African Elephant.", "answer": "False"},
+{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+{"text": "Google was originally called 'Backrub'.", "answer": "True"},
+{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+question_data2 = [
+    {"text": "The Great Wall of China is visible from space.", "answer": "False"},
+    {"text": "Sharks are mammals.", "answer": "False"},
+    {"text": "The human body has 206 bones.", "answer": "True"},
+    {"text": "Lightning never strikes the same place twice.", "answer": "False"},
+    {"text": "Bananas grow on trees.", "answer": "False"},
+    {"text": "The Pacific Ocean is the largest ocean on Earth.", "answer": "True"},
+    {"text": "Penguins can fly.", "answer": "False"},
+    {"text": "Water boils at 100 degrees Celsius at sea level.", "answer": "True"},
+    {"text": "The capital of Australia is Sydney.", "answer": "False"},
+    {"text": "Mount Everest is the tallest mountain above sea level.", "answer": "True"},
+    {"text": "Bats are blind.", "answer": "False"},
+    {"text": "An octopus has three hearts.", "answer": "True"},
+    {"text": "The Sun is a planet.", "answer": "False"},
+    {"text": "Honey never spoils.", "answer": "True"},
+    {"text": "Humans can breathe and swallow at the same time.", "answer": "False"}
+]
