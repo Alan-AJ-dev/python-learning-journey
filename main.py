f1d49380091ba7371934fb68d074bf590acfe8c6
Alan Joseph <alanjoseph52006@gmail.com>
2026-06-18 14:45:37 +0530

    Enhance password manager UI with improved spacing and alignment

--- main.py
+++ main.py
@@ -1,5 +1,58 @@
+from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# ---------------- Canvas ---------------- #
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+lock = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock)
+canvas.grid(row=0, column=1)
+
+# ---------------- Labels ---------------- #
+
+Label(text="Website:").grid(row=1, column=0, sticky="E", pady=5)
+
+Label(text="Email/Username:").grid(row=2, column=0, sticky="E", pady=5)
+
+Label(text="Password:").grid(row=3, column=0, sticky="E", pady=5)
+
+# ---------------- Entries ---------------- #
+
+website_entry = Entry(width=36)
+website_entry.grid(row=1, column=1, columnspan=2, sticky="W")
+website_entry.focus()
+
+email_entry = Entry(width=36)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="W")
+email_entry.insert(0, "alanjoseph52006@gmail.com")
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1, sticky="W")
+
+# ---------------- Buttons ---------------- #
+
+generate_btn = Button(
+    text="Generate Password",
+    width=15
+)
+generate_btn.grid(row=3, column=2, padx=(5,0),sticky="W")
+
+add_btn = Button(
+    text="Add",
+    width=36,
+    command=save_data
+)
+add_btn.grid(row=4, column=1, columnspan=2, pady=10,sticky="W")
+
+window.mainloop()
+window.mainloop()
