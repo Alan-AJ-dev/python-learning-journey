de8136c5ed9d7d45128c3ff7f040fafecb555e6e
Alan Joseph <alanjoseph52006@gmail.com>
2026-07-18 11:05:27 +0530

    Updated the quiz_brain logic that helps compatable with ui

--- Oops/quiz-game-start/quiz_brain.py
+++ Oops/quiz-game-start/quiz_brain.py
@@ -1,40 +1,31 @@
 from data import question_data
 from question_model import Question
 import  html
+
 class QuizBrain():
     def __init__(self,ques_list):
         self.question_number = 0
         self.score = 0
+        self.is_over = False
         self.question_list = ques_list
-        
+        self.current_question = None
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
-        self.question_number += 1
-        in_ans =  input(f"Q: {self.question_number}: {html.unescape(current_question.text)} (True/False): ")
-        if self.check_ans(current_question.ans,in_ans):
-            self.score +=1
-            print(f"Your answer is correct" )
-        elif in_ans == "exit":
-            print("Exiting as your wish")
-            return True
-        else:
-            print("Answer Was Wrong !!!")
-            print(f"The correct ans was {current_question.ans}")
-        print(f"Your score is ({self.score}")
-        print(" "*2)
-        return False
+        try:
 
-    def still_remain(self):
-       
-        if self.question_number < len(self.question_list):
-            return True
-        else:
-            print("Questions Over")
-            return False
+            self.current_question = self.question_list[self.question_number]
+            self.question_number += 1
+        except IndexError:
+            self.is_over = True
+
+
+        return f"{self.question_number} {html.unescape(self.current_question.text)}"
+
+
+
     
 
-    def check_ans(self,question_ans,in_ans):
-        return question_ans.lower() == in_ans.lower()
+    def check_ans(self,in_ans):
+        return self.current_question.ans.lower() == in_ans.lower()
     
 
